2dcf9b6f786e0b193e917dfa6cda8c082f412cdf
Thiago Lima <thiagofslima@gmail.com>
2025-07-22 12:53:51 -0300

    feat: add endereço em pessoas

--- GestaoPedidosWpf/Models/Pessoa.cs
+++ GestaoPedidosWpf/Models/Pessoa.cs
@@ -5,10 +5,27 @@
         public int Id { get; set; }
         public string Nome { get; set; }
         public string Cpf { get; set; }
+        public Endereco Endereco { get; set; } = new Endereco();
 
         public override string ToString()
         {
             return $"#{Id} - {Nome} - {Cpf}";
+        }
+    }
+
+    public class Endereco
+    {
+        public int Id { get; set; }
+        public string Rua { get; set; }
+        public string Numero { get; set; }
+        public string Bairro { get; set; }
+        public string Cidade { get; set; }
+        public string Estado { get; set; }
+        public string Cep { get; set; }
+
+        override public string ToString()
+        {
+            return $"{Rua}, {Numero} - {Bairro}, {Cidade} - {Estado}, {Cep}";
         }
     }
 }
